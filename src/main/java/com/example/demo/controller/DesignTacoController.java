1e682de4f5625ac86d8bd334aecbf8c9d6fb838b
nichm <nichmihai4@gmail.com>
2024-10-07 23:47:39 -0400

    Added JDBC to the project, also created Repository

--- src/main/java/com/example/demo/controller/DesignTacoController.java
+++ src/main/java/com/example/demo/controller/DesignTacoController.java
@@ -2,15 +2,21 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.Ingredient;
 import com.example.demo.model.Taco;
+import com.example.demo.repository.IngredientRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.domain.Ingredient.Type;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,22 +24,20 @@ import java.util.stream.Collectors;
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository) {
+        this.ingredientRepository = ingredientRepository;
+    }
 
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatos", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(i -> ingredients.add(i));
 
         Type[] types = Ingredient.Type.values();
         for (Type type : types) {
@@ -47,7 +51,12 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+
+        if(errors.hasErrors()) {
+            return "design";
+        }
+
         log.info("Processing design: " + design);
 
         return "redirect:/orders/current";
--- src/main/java/com/example/demo/model/Taco.java
+++ src/main/java/com/example/demo/model/Taco.java
@@ -1,12 +1,19 @@
 package com.example.demo.model;
 
-import com.example.demo.domain.Ingredient;
 import lombok.Data;
-
+import javax.validation.constraints.Size;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+
+    private Long id;
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
+    @Size(min = 1, message = "You must chose at least 1 ingredient")
     private List<String> ingredients;
+    private Date createdAt;
 }
